70cec7ab4976d4ebc2f1a97367e9c53ab76fec09
Lucas <lucasdpau@gmail.com>
2020-07-06 10:52:05 -0400

    form properly adds city name and number of days to the reactDom city list

--- mainapp/static/app1/index.js
+++ mainapp/static/app1/index.js
@@ -6,50 +6,81 @@ class UiWrapper extends React.Component {
         super(props);
         this.state = {
             cityName: '',
-            citiesList: ['asd', 'asdas'],
-            days: 0
+            citiesList: [],
+            days: 1,
         };
+        this.handleChange = this.handleChange.bind(this);
+    }
+    
+    handleChange(event) {
+        this.setState({[event.target.name]: event.target.value,
+                        });
+        console.log("cityName is " + this.state.cityName);
+        console.log('days is ' + this.state.days);
     }
 
-    addCities = (city) => {
+    addCities = () => {
         // update the city list by adding a new city
-        var newCitiesList = this.state.citiesList;
-        newCitiesList.push(city);
-        this.setState({citiesList: newCitiesList});
+        let newCitiesList = this.state.citiesList;
+        let city = this.state.cityName;
+        for (let i=0; i<this.state.days; i++) {
+            newCitiesList.push(city);
+            console.log(i + " " + city);
+        }
+        this.setState({citiesList: newCitiesList, days: 1, cityName: ''});
     }
 
     render() {
         return (
             //React.createElement('div', null, 'qq')
             Ele('div', null, 
-                Ele(cityAddForm, {}),
+                Ele(cityAddForm, {addCities: this.addCities, 
+                    cityName: this.cityName,
+                    days: this.days,
+                    handleChange: this.handleChange}),
                 Ele('div', null, `${this.state.citiesList}`), 
-                Ele(cityAddButton, {addCities: this.addCities}),
                 )
         );
     }
 }
 
-function cityAddForm(props) {
-    return (
-        Ele('form', {}, 
-            Ele('label', {'htmlFor':'city'}, 'City'),
-            Ele('input', {'type': 'text', 'name': 'city', 'id':'city'}),
-            Ele('br',null,null),
-            Ele('label', {'htmlFor':'days_in_city'}, 'Days in City'),
-            Ele('input', {'type': 'number', 'name': 'days_in_city', 'id':'days_in_city', 'min': '1'}),
-            Ele('br', null, null),
-            Ele('input', {'type': 'button', 'id':'addCityButton', 'value': 'Add City'}),
-            Ele('br', null, null),
-            Ele('input', {'type': 'submit'}),
-
-        )
-    );
+class cityAddForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+    render() {
+        return (
+            Ele('form', {}, 
+                Ele('label', {'htmlFor':'cityName'}, 'City'),
+                Ele('input', {'type': 'text', 
+                            'name': 'cityName',
+                            'id':'cityName',
+                            'value': this.props.cityName,
+                            'onChange': this.props.handleChange, 
+                            }),
+                Ele('br',null,null),
+                Ele('label', {'htmlFor':'days'}, 'Days in City'),
+                Ele('input', {'type': 'number', 
+                            'name': 'days', 
+                            'id':'days', 
+                            'min': '1', 
+                            'value': this.props.days,
+                            'onChange': this.props.handleChange,
+                        }),
+                Ele('br', null, null),
+                Ele(cityAddButton, {addCities: this.props.addCities}),
+                Ele('br', null, null),
+                Ele('input', {'type': 'submit'}),
+            )
+        );
+    }
 }
 
 function cityAddButton(props) {
     return(
-        Ele('button', {onClick: () => props.addCities('toronto')},'asd')
+        Ele('button', {'type': 'button', onClick: () => props.addCities()},'Add City')
     );
 }
 
@@ -57,23 +88,3 @@ ReactDOM.render(
   React.createElement(UiWrapper, null),
   document.getElementById('root')
 );
-
-
-var cityList = [];
-
-const addCityButton = document.getElementById("addCityButton");
-const cityInput = document.getElementById("city");
-const daysInCityInput = document.getElementById('days_in_city');
-
-addCityButton.addEventListener('click', addCity);
-function addCity() {
-    var cityName = cityInput.value;
-    var daysInCity = daysInCityInput.value;
-    for (let i = 0; i < daysInCity; i++) {
-        cityList.push(cityName);
-    }
-    // reset the form
-    cityInput.value = '';
-    daysInCityInput.value = 1;
-
-}
